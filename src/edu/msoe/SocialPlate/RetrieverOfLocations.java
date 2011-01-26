ae6b9017a3feb0fe6ab91760e0ef3ab2b98e60f6
Michael Neidinger <michael@linux-you3.site>
2011-01-26 12:48:20 -0600

    Added place holder classes to get a jumpstart on development.

---
+++ src/edu/msoe/SocialPlate/RetrieverOfLocations.java
@@ -0,0 +1,33 @@
+package edu.msoe.SocialPlate;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+public class RetrieverOfLocations implements LocationListener{
+
+	
+	public void onLocationChanged(Location arg0) {
+
+		
+	}
+
+	
+	public void onProviderDisabled(String arg0) {
+
+		
+	}
+
+	
+	public void onProviderEnabled(String arg0) {
+
+		
+	}
+
+	
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+
+		
+	}
+
+}
