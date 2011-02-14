15f38bc16e63a1d5bce5ee58c6505f0bc16baa75
unknown <KRIS@.(none)>
2011-02-13 18:03:56 -0600

    6pm sunday

--- gen/edu/msoe/SocialPlate/R.java
+++ gen/edu/msoe/SocialPlate/R.java
@@ -13,25 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int america=0x7f020000;
         public static final int brazil=0x7f020001;
-        public static final int breakfast=0x7f020002;
-        public static final int china=0x7f020003;
-        public static final int colombia=0x7f020004;
-        public static final int cost=0x7f020005;
-        public static final int cost1=0x7f020006;
-        public static final int cost2=0x7f020007;
-        public static final int cost3=0x7f020008;
-        public static final int dinner=0x7f020009;
-        public static final int earth=0x7f02000a;
-        public static final int eraser=0x7f02000b;
-        public static final int ethiopia=0x7f02000c;
-        public static final int geotag=0x7f02000d;
-        public static final int icon=0x7f02000e;
-        public static final int latenight=0x7f02000f;
-        public static final int lunch=0x7f020010;
-        public static final int map=0x7f020011;
-        public static final int meal=0x7f020012;
-        public static final int plate=0x7f020013;
-        public static final int search=0x7f020014;
+        public static final int china=0x7f020002;
+        public static final int colombia=0x7f020003;
+        public static final int cost=0x7f020004;
+        public static final int cost1=0x7f020005;
+        public static final int cost2=0x7f020006;
+        public static final int cost3=0x7f020007;
+        public static final int earth=0x7f020008;
+        public static final int eraser=0x7f020009;
+        public static final int ethiopia=0x7f02000a;
+        public static final int geotag=0x7f02000b;
+        public static final int icon=0x7f02000c;
+        public static final int map=0x7f02000d;
+        public static final int meal=0x7f02000e;
+        public static final int plate=0x7f02000f;
+        public static final int search=0x7f020010;
     }
     public static final class id {
         public static final int clearallbutton=0x7f060013;
